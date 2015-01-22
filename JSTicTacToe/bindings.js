b970fee08565f45635e60550d3dda551ad626ed0
hassanhabib <hassanhabib@live.com>
2015-01-22 03:24:58 -0600

    Put some comments to describ how the functionality in the bindings file will looks like.

--- JSTicTacToe/bindings.js
+++ JSTicTacToe/bindings.js
@@ -1,1 +1,2 @@
-﻿
+﻿// This file will contain functions that pull the current status of the TicTacToe tables.
+// so if the table has an X that will be represented as 0 and if it is O that will be represented as 1
