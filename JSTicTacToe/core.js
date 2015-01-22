529952028d02f634a50fc5a224e9bdfc28b96bf9
hassanhabib <hassanhabib@live.com>
2015-01-22 04:01:03 -0600

    added a sample 3 dimensional array for testing purposes.

--- JSTicTacToe/core.js
+++ JSTicTacToe/core.js
@@ -1,4 +1,25 @@
-﻿function NextStep(x, y, z) {
+﻿
+
+/* This piece is for testing purposes*/
+var inputArr = new Array(3);
+for (var i = 0; i < 3; i++) {
+    inputArr[i] = new Array(3);
+    for (var j = 0; j < 3; j++) {
+        inputArr[i][j] = new Array(3);
+    }
+}
+
+inputArr[0][0][0] = 0;
+inputArr[0][0][1] = 0;
+inputArr[0][0][2] = 0;
+inputArr[0][1][0] = 0;
+inputArr[0][1][1] = 0;
+inputArr[0][1][2] = 0;
+inputArr[0][2][0] = 0;
+inputArr[0][2][1] = 0;
+inputArr[0][2][2] = 0;
+
+function NextStep(x, y, z) {
     array1 = [x, y, z];
     array2 = [x, y, z];
     array3 = [x, y, z];
