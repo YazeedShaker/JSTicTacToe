046edfad756025dba6888c8126d5e6c8a72b8099
YazeedShaker <yazeed.shaker@outlook.com>
2015-01-26 21:02:37 +0200

    JS file UPDATE

--- JSTicTacToe/core.js
+++ JSTicTacToe/core.js
@@ -1,4 +1,9 @@
-﻿function nextStep(arrayTotal, choice) {
+﻿// (0) is for O
+// (1) is for X 
+// (2) is for a dash (-) --> a blank place
+// the choice is the player's character and the result array is what you should do to block 
+
+function nextStep(arrayTotal, choice) {
 
     this.arrayTotal = arrayTotal;
     var arrayX = [];
@@ -8,7 +13,7 @@
     secondRow = [arrayTotal[3], arrayTotal[4], arrayTotal[5]];
     thirdRow = [arrayTotal[6], arrayTotal[7], arrayTotal[8]];
 
-    //for there is already a winning case in the given input array 
+    //for there is already a winning case in the given input array we change it 
     if (((firstRow[0] === firstRow[1]) && (firstRow[1] === firstRow[2])) || ((firstRow[0] === secondRow[0]) && (secondRow[0] === thirdRow[0])) || ((firstRow[0] === secondRow[1]) && (secondRow[1] === thirdRow[2]))) {
         firstRow[0] = 2;
     } if (((firstRow[2] === secondRow[2]) && (secondRow[2] === thirdRow[2])) || ((firstRow[2] === secondRow[1]) && (secondRow[1] === thirdRow[0]))) {
@@ -22,7 +27,7 @@
         //For the case if the choice is X
         case "x":
 
-            //for the probability of a winning case
+            //for the probability of a winning case we block it with an O
             if (((firstRow[1] === firstRow[2]) && (firstRow[2] === 1) || (secondRow[1] === thirdRow[2]) && (thirdRow[2] === 1)) || ((secondRow[0] === thirdRow[0]) && (thirdRow[0] === 1))) {
                 arrayO.push(1);
             } if (((secondRow[1] === thirdRow[0]) && (thirdRow[0] === 1)) || ((secondRow[2] === thirdRow[2]) && (thirdRow[2] === 1))) {
@@ -52,7 +57,7 @@
             }
             break;
 
-            //For the case if the choice is O
+            //For the case if the choice is O we block it with an X
         case "o":
             //for the probability of a winning case
             if (((firstRow[1] === firstRow[2]) && (firstRow[2] === 0) || (secondRow[1] === thirdRow[2]) && (thirdRow[2] === 0)) || ((secondRow[0] === thirdRow[0]) && (thirdRow[0] === 0))) {
