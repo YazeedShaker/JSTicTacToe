da71df60b559a677694839ce3f9a0360a3775ea1
YazeedShaker <yazeed.shaker@outlook.com>
2015-02-04 01:28:27 +0200

    JS File

--- JSTicTacToe/core.js
+++ JSTicTacToe/core.js
@@ -1,198 +1,163 @@
-﻿// (0) is for O
-// (1) is for X 
-// (2) is for a dash (-) --> a blank place
-// the choice is the player's character and the result array is what you should do to block 
-
-function nextStep(arrayTotal, choice) {
-
-    this.arrayTotal = arrayTotal;
+﻿function nextStep(first, choice) {
     var arrayX = [];
     var arrayO = [];
-
-    firstRow = [arrayTotal[0], arrayTotal[1], arrayTotal[2]];
-    secondRow = [arrayTotal[3], arrayTotal[4], arrayTotal[5]];
-    thirdRow = [arrayTotal[6], arrayTotal[7], arrayTotal[8]];
-
-    //for there is already a winning case in the given input array we change it by (2) 
-    if (((firstRow[0] === firstRow[1]) && (firstRow[1] === firstRow[2])) || ((firstRow[0] === secondRow[0]) && (secondRow[0] === thirdRow[0])) || ((firstRow[0] === secondRow[1]) && (secondRow[1] === thirdRow[2]))) {
-        firstRow[0] = 2;
-    } if (((firstRow[2] === secondRow[2]) && (secondRow[2] === thirdRow[2])) || ((firstRow[2] === secondRow[1]) && (secondRow[1] === thirdRow[0]))) {
-        firstRow[2] = 2;
-    } if ((firstRow[1] === secondRow[1]) && (secondRow[1] === thirdRow[1])) {
-        firstRow[1] = 2;
+    var second = [];
+    var check = [3, 1, 0, 6, 4, 0, 9, 7];
+    var check2 = [7, 1, 0, 0, 8, 2, 0, 9, 3, 0];
+    var check3 = [4, 0, 5, 0, 6];
+    var check4 = [2, 5, 8];
+    var check5 = [9, 1];
+    var check6 = [7, 3];
+    var j = 0;
+    //for The second array
+    for (var i = 0; i < first.length - 1; i++) {
+        second[i] = first[i + 1];
     }
-
-
-    switch (choice) {
-        //For the case if the choice is X
-        case "x":
-
-            //for the winning case  
-            if (((firstRow[1] === firstRow[2]) && (firstRow[2] === 1) && (firstRow[0] === 2) || (secondRow[1] === thirdRow[2]) && (thirdRow[2] === 1)) && (firstRow[0] === 2) || ((secondRow[0] === thirdRow[0]) && (thirdRow[0] === 1)) && (firstRow[0] === 2)) {
-                arrayX.push(1);
-            } if (((secondRow[1] === thirdRow[0]) && (thirdRow[0] === 1)) && (firstRow[2] === 2) || ((secondRow[2] === thirdRow[2]) && (thirdRow[2] === 1)) && (firstRow[2] === 2)) {
-                arrayX.push(3);
-            } if ((firstRow[1] === secondRow[1]) && (secondRow[1] === 1) && (thirdRow[1] === 2)) {
-                arrayX.push(8);
-            } if ((secondRow[1] === thirdRow[1]) && (thirdRow[1] === 1) && (firstRow[1] === 2)) {
-                arrayX.push(2);
-            } if ((firstRow[1] === thirdRow[1]) && (thirdRow[1] === 1) && (secondRow[1]) === 2) {
-                arrayX.push(5);
-            } if ((secondRow[0] === secondRow[1]) && (secondRow[1] === 1) && (secondRow[2] === 2)) {
-                arrayX.push(6);
-            } if ((secondRow[0] === secondRow[2]) && (secondRow[2] === 1) && (secondRow[1] === 2)) {
-                arrayX.push(5);
-            } if ((secondRow[1] === secondRow[2]) && (secondRow[2] === 1) && (secondRow[0] === 2)) {
-                arrayX.push(4);
-            } if ((thirdRow[0] === thirdRow[1]) && (thirdRow[1] === 1) && (thirdRow[2]) === 2) {
-                arrayX.push(9);
-            } if ((thirdRow[0] === thirdRow[2]) && (thirdRow[2] === 1) && (thirdRow[1] === 2)) {
-                arrayX.push(8);
-            } if ((thirdRow[1] === thirdRow[2]) && (thirdRow[2] === 1) && (thirdRow[0] === 2)) {
-                arrayX.push(7);
-            } if ((firstRow[0] === secondRow[0]) && (secondRow[0] === 1) && (thirdRow[0]) === 2) {
-                arrayX.push(7);
-            } if ((firstRow[0] === secondRow[1]) && (secondRow[1] === 1) && (thirdRow[2] === 2)) {
-                arrayX.push(9);
-            } if ((firstRow[2] === secondRow[1]) && (secondRow[1] === 1) && (thirdRow[0] === 2)) {
-                arrayX.push(7);
-            } if ((firstRow[0] === firstRow[2]) && (firstRow[2] === 1) && (firstRow[1]) === 2) {
-                arrayX.push(2);
-            } if ((firstRow[0] === thirdRow[2]) && ((thirdRow[2]) === 1) && (secondRow[1]) === 2) {
-                arrayX.push(5);
-            } if ((firstRow[2] === thirdRow[2]) && (thirdRow[2] === 1) && (secondRow[2] === 2)) {
-                arrayX.push(6);
-            }
-
-            //For the blocknig case
-            if (((firstRow[1] === firstRow[2]) && (firstRow[2] === 0) && (firstRow[0] === 2) || (secondRow[1] === thirdRow[2]) && (thirdRow[2] === 0)) && (firstRow[0] === 2) || ((secondRow[0] === thirdRow[0]) && (thirdRow[0] === 0)) && (firstRow[0] === 2)) {
-                arrayX.push(1);
-            } if (((secondRow[1] === thirdRow[0]) && (thirdRow[0] === 0)) && (firstRow[2] === 2) || ((secondRow[2] === thirdRow[2]) && (thirdRow[2] === 0)) && (firstRow[2] === 2)) {
-                arrayX.push(3);
-            } if ((firstRow[1] === secondRow[1]) && (secondRow[1] === 0) && (thirdRow[1] === 2)) {
-                arrayX.push(8);
-            } if ((secondRow[1] === thirdRow[1]) && (thirdRow[1] === 0) && (firstRow[1] === 2)) {
-                arrayX.push(2);
-            } if ((firstRow[1] === thirdRow[1]) && (thirdRow[1] === 0) && (secondRow[1] === 2)) {
-                arrayX.push(5);
-            } if ((secondRow[0] === secondRow[1]) && (secondRow[1] === 0) && (secondRow[2] === 2)) {
-                arrayX.push(6);
-            } if ((secondRow[0] === secondRow[2]) && (secondRow[2] === 0) && (secondRow[1] === 2)) {
-                arrayX.push(5);
-            } if ((secondRow[1] === secondRow[2]) && (secondRow[2] === 0) && (secondRow[0] === 2)) {
-                arrayX.push(4);
-            } if ((thirdRow[0] === thirdRow[1]) && (thirdRow[1] === 0) && (thirdRow[2] === 2)) {
-                arrayX.push(9);
-            } if ((thirdRow[0] === thirdRow[2]) && (thirdRow[2] === 0) && (thirdRow[1] === 2)) {
-                arrayX.push(8);
-            } if ((thirdRow[1] === thirdRow[2]) && (thirdRow[2] === 0) && (thirdRow[0] === 2)) {
-                arrayX.push(7);
-            } if ((firstRow[0] === secondRow[0]) && (secondRow[0] === 0) && (thirdRow[0] === 2)) {
-                arrayX.push(7);
-            } if ((firstRow[0] === secondRow[1]) && (secondRow[1] === 0) && (thirdRow[2] === 2)) {
-                arrayX.push(9);
-            } if ((firstRow[2] === secondRow[1]) && (secondRow[1] === 0) && (thirdRow[0] === 2)) {
-                arrayX.push(7);
-            } if ((firstRow[0] === firstRow[2]) && (firstRow[2] === 0) && (firstRow[1]) === 2) {
-                arrayX.push(2);
-            } if ((firstRow[0] === thirdRow[2]) && ((thirdRow[2]) === 0) && (secondRow[1]) === 2) {
-                arrayX.push(5);
-            } if ((firstRow[2] === thirdRow[2]) && (thirdRow[2] === 0) && (secondRow[2] === 2)) {
-                arrayX.push(6);
-            }
-            break;
-
-        case "o":
-            //for the winning case 
-            if (((firstRow[1] === firstRow[2]) && (firstRow[2] === 0) && (firstRow[0] === 2) || (secondRow[1] === thirdRow[2]) && (thirdRow[2] === 0)) && (firstRow[0] === 2) || ((secondRow[0] === thirdRow[0]) && (thirdRow[0] === 0)) && (firstRow[0] === 2)) {
-                arrayO.push(1);
-            } if (((secondRow[1] === thirdRow[0]) && (thirdRow[0] === 0)) && (firstRow[2] === 2) || ((secondRow[2] === thirdRow[2]) && (thirdRow[2] === 0)) && (firstRow[2] === 2)) {
-                arrayO.push(3);
-            } if ((firstRow[1] === secondRow[1]) && (secondRow[1] === 0) && (thirdRow[1] === 2)) {
-                arrayO.push(8);
-            } if ((secondRow[1] === thirdRow[1]) && (thirdRow[1] === 0) && (firstRow[1] === 2)) {
-                arrayO.push(2);
-            } if ((firstRow[1] === thirdRow[1]) && (thirdRow[1] === 0) && (secondRow[1]) === 2) {
-                arrayO.push(5);
-            } if ((secondRow[0] === secondRow[1]) && (secondRow[1] === 0) && (secondRow[2] === 2)) {
-                arrayO.push(6);
-            } if ((secondRow[0] === secondRow[2]) && (secondRow[2] === 0) && (secondRow[1] === 2)) {
-                arrayO.push(5);
-            } if ((secondRow[1] === secondRow[2]) && (secondRow[2] === 0) && (secondRow[0] === 2)) {
-                arrayO.push(4);
-            } if ((thirdRow[0] === thirdRow[1]) && (thirdRow[1] === 0) && (thirdRow[2]) === 2) {
-                arrayO.push(9);
-            } if ((thirdRow[0] === thirdRow[2]) && (thirdRow[2] === 0) && (thirdRow[1] === 2)) {
-                arrayO.push(8);
-            } if ((thirdRow[1] === thirdRow[2]) && (thirdRow[2] === 0) && (thirdRow[0] === 2)) {
-                arrayO.push(7);
-            } if ((firstRow[0] === secondRow[0]) && (secondRow[0] === 0) && (thirdRow[0]) === 2) {
-                arrayO.push(7);
-            } if ((firstRow[0] === secondRow[1]) && (secondRow[1] === 0) && (thirdRow[2] === 2)) {
-                arrayO.push(9);
-            } if ((firstRow[2] === secondRow[1]) && (secondRow[1] === 0) && (thirdRow[0] === 2)) {
-                arrayO.push(7);
-            } if ((firstRow[0] === firstRow[2]) && (firstRow[2] === 0) && (firstRow[1]) === 2) {
-                arrayO.push(2);
-            } if ((firstRow[0] === thirdRow[2]) && ((thirdRow[2]) === 0) && (secondRow[1]) === 2) {
-                arrayO.push(5);
-            } if ((firstRow[2] === thirdRow[2]) && (thirdRow[2] === 0) && (secondRow[2] === 2)) {
-                arrayO.push(6);
-            }
-            //For the blocking case
-
-            if (((firstRow[1] === firstRow[2]) && (firstRow[2] === 1) && (firstRow[0] === 2) || (secondRow[1] === thirdRow[2]) && (thirdRow[2] === 1)) && (firstRow[0] === 2) || ((secondRow[0] === thirdRow[0]) && (thirdRow[0] === 1)) && (firstRow[0] === 2)) {
-                arrayO.push(1);
-            } if (((secondRow[1] === thirdRow[0]) && (thirdRow[0] === 1)) && (firstRow[2] === 2) || ((secondRow[2] === thirdRow[2]) && (thirdRow[2] === 1)) && (firstRow[2] === 2)) {
-                arrayO.push(3);
-            } if ((firstRow[1] === secondRow[1]) && (secondRow[1] === 1) && (thirdRow[1] === 2)) {
-                arrayO.push(8);
-            } if ((secondRow[1] === thirdRow[1]) && (thirdRow[1] === 1) && (firstRow[1] === 2)) {
-                arrayO.push(2);
-            } if ((firstRow[1] === thirdRow[1]) && (thirdRow[1] === 1) && (secondRow[1] === 2)) {
-                arrayO.push(5);
-            } if ((secondRow[0] === secondRow[1]) && (secondRow[1] === 1) && (secondRow[2] === 2)) {
-                arrayO.push(6);
-            } if ((secondRow[0] === secondRow[2]) && (secondRow[2] === 1) && (secondRow[1] === 2)) {
-                arrayO.push(5);
-            } if ((secondRow[1] === secondRow[2]) && (secondRow[2] === 1) && (secondRow[0] === 2)) {
-                arrayO.push(4);
-            } if ((thirdRow[0] === thirdRow[1]) && (thirdRow[1] === 1) && (thirdRow[2] === 2)) {
-                arrayO.push(9);
-            } if ((thirdRow[0] === thirdRow[2]) && (thirdRow[2] === 1) && (thirdRow[1] === 2)) {
-                arrayO.push(8);
-            } if ((thirdRow[1] === thirdRow[2]) && (thirdRow[2] === 1) && (thirdRow[0] === 2)) {
-                arrayO.push(7);
-            } if ((firstRow[0] === secondRow[0]) && (secondRow[0] === 1) && (thirdRow[0] === 2)) {
-                arrayO.push(7);
-            } if ((firstRow[0] === secondRow[1]) && (secondRow[1] === 1) && (thirdRow[2] === 2)) {
-                arrayO.push(9);
-            } if ((firstRow[2] === secondRow[1]) && (secondRow[1] === 1) && (thirdRow[0] === 2)) {
-                arrayO.push(7);
-            } if ((firstRow[0] === firstRow[2]) && (firstRow[2] === 1) && (firstRow[1]) === 2) {
-                arrayO.push(2);
-            } if ((firstRow[0] === thirdRow[2]) && ((thirdRow[2]) === 1) && (secondRow[1]) === 2) {
-                arrayO.push(5);
-            } if ((firstRow[2] === thirdRow[2]) && (thirdRow[2] === 1) && (secondRow[2] === 2)) {
-                arrayO.push(6);
-            }
-
-            break;
-
-        default:
-            console.log("Please enter a valid choice of X or O");
-    }
-
 
     if (choice === "o") {
-        console.log(_.uniq(arrayO, false));
+
+        //for the horizontal cells
+        for (var i = 0; i < second.length; i++) {
+            if (first[i] === 2 || first[i] === 5) {
+                null;
+            }
+            if ((first[i] === second[i]) && (second[i] === 0)) {
+                if (first[check[i] - 1] === 2)
+                    arrayO.push(check[i]);
+            }
+        }
+        //for the vertical cells 
+        for (var i = 0; i < 3; i++) {
+            for (var k = i; k <= first.length; k += 3) {
+                if ((first[k] === second[k + 2]) && (second[k + 2] === 0)) {
+                    if (first[check2[j] - 1] === 2)
+                        arrayO.push(check2[j]);
+                }
+                j++;
+            }
+        }
+        //return j=0
+        j = 0;
+        //for vertical first & last
+        for (var i = 0; i < 3; i++) {
+            for (var k = i; k <= first.length; k += 6) {
+                if ((first[k] === second[k + 5]) && (second[k + 5] === 0)) {
+                    if (first[check3[j] - 1] === 2)
+                        arrayO.push(check3[j]);
+                }
+                j++;
+            }
+        }
+        //return j=0
+        j = 0;
+        //for horizontal first & last
+        for (var i = 0; i <= first.length; i += 3) {
+            if ((first[i] === second[i + 1]) && (second[i + 1] === 0)) {
+                if (first[check4[j] - 1] === 2)
+                    arrayO.push(check4[j]);
+            }
+            j++;
+        }
+        //return j=0
+        j = 0;
+        //for diagonal first[0]
+        for (var i = 0; i < 5; i += 4) {
+            if ((first[i] === second[i + 3]) && (second[i + 3] === 0)) {
+                if (first[check5[j] - 1] === 2)
+                    arrayO.push(check5[j]);
+            }
+            j++;
+        }
+        //return j=0
+        j = 0;
+        //for diagonal first[2]
+        for (var i = 2; i < 5; i += 2) {
+            if ((first[i] === second[i + 1]) && (second[i + 1] === 0)) {
+                if (first[check6[j] - 1] === 2)
+                    arrayO.push(check6[j]);
+            }
+            j++;
+        }
+        //for the diagonal first & last two cases
+        if (((first[0] === second[7]) && (second[7] === 0) && (first[4] === 2)) || ((first[2] === second[5]) && (second[5] === 0) && (first[4] === 2))) {
+            arrayO.push(5);
+        }
+        // for the X player   
     } else {
-        console.log(_.uniq(arrayX, false));
+        //for the horizontal cells
+        for (var i = 0; i < second.length; i++) {
+            if (first[i] === 2 || first[i] === 5) {
+                null;
+            }
+            if ((first[i] === second[i]) && (second[i] === 1)) {
+                if (first[check[i] - 1] === 2)
+                    arrayX.push(check[i]);
+            }
+        }
+        //for the vertical cells 
+        for (var i = 0; i < 3; i++) {
+            for (var k = i; k <= first.length; k += 3) {
+                if ((first[k] === second[k + 2]) && (second[k + 2] === 1)) {
+                    if (first[check2[j] - 1] === 2)
+                        arrayX.push(check2[j]);
+                }
+                j++;
+            }
+        }
+        //return j=0
+        j = 0;
+        //for vertical first & last
+        for (var i = 0; i < 3; i++) {
+            for (var k = i; k <= first.length; k += 6) {
+                if ((first[k] === second[k + 5]) && (second[k + 5] === 1)) {
+                    if (first[check3[j] - 1] === 2)
+                        arrayX.push(check3[j]);
+                }
+                j++;
+            }
+        }
+        //return j=0
+        j = 0;
+        //for horizontal first & last
+        for (var i = 0; i <= first.length; i += 3) {
+            if ((first[i] === second[i + 1]) && (second[i + 1] === 0)) {
+                if (first[check4[j] - 1] === 2)
+                    arrayX.push(check4[j]);
+            }
+            j++;
+        }
+        //return j=0
+        j = 0;
+        //for diagonal first[0]
+        for (var i = 0; i < 5; i += 4) {
+            if ((first[i] === second[i + 3]) && (second[i + 3] === 1)) {
+                if (first[check5[j] - 1] === 2)
+                    arrayX.push(check5[j]);
+            }
+            j++;
+        }
+        //return j=0
+        j = 0;
+        //for diagonal first[2]
+        for (var i = 2; i < 5; i += 2) {
+            if ((first[i] === second[i + 1]) && (second[i + 1] === 1)) {
+                if (first[check6[j] - 1] === 2)
+                    arrayX.push(check6[j]);
+            }
+            j++;
+        }
+        //for the diagonal first & last two cases
+        if (((first[0] === second[7]) && (second[7] === 1) && (first[4] === 2)) || ((first[2] === second[5]) && (second[5] === 1) && (first[4] === 2))) {
+            arrayX.push(5);
+        }
     }
-
+    if (choice === "o") {
+        console.log(arrayO[0]);
+    } else {
+        console.log(arrayX[0]);
+    }
 }
-
-
-var array = [1, 2, 0, 0, 2, 2, 1, 2, 1];
-nextStep.arrayTotal = array;
+var array = [2, 2, 1, 2, 2, 2, 0, 2, 2];
+nextStep.first = array;
 nextStep(array, "x");
+nextStep(array, "o");
