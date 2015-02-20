e755ab5b07565968fb351b1da91d2e01eb3d34c5
YazeedShaker <yazeed.shaker@outlook.com>
2015-02-21 00:03:41 +0200

    addingJQuery

--- JSTicTacToe/bindings.js
+++ JSTicTacToe/bindings.js
@@ -1,2 +1,8 @@
 ﻿// This file will contain functions that pull the current status of the TicTacToe tables.
 // so if the table has an X that will be represented as 0 and if it is O that will be represented as 1
+
+$(document).ready(function () {
+    $("td,div").click(function () {
+        $(this).text("x");
+    });
+});
